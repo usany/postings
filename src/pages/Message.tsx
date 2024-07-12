863d0763f96bf00bdd1c7235bd268c053bdd1cc0
ChangbeomAhn <changbeomahn@ChangbeomAhnui-Macmini.local>
2024-07-13 03:47:55 +0900

    add

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
 import { auth, onSocialClick, dbservice, storage } from 'src/baseApi/serverbase'
 import { collection, query, where, orderBy, addDoc, getDocs, doc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore';
-import Btn from 'src/pages/Btn';
-import Avatars from 'src/muiComponents/Avatars';
-import FastRewindIcon from '@mui/icons-material/FastRewind';
-import { BrowserRouter, Routes, Route, useNavigate, Link } from 'react-router-dom'
-import Papers from 'src/muiComponents/Papers';
+// import Btn from 'src/pages/Btn';
+// import Avatars from 'src/muiComponents/Avatars';
+// import FastRewindIcon from '@mui/icons-material/FastRewind';
+// import { BrowserRouter, Routes, Route, useNavigate, Link } from 'react-router-dom'
+// import Papers from 'src/muiComponents/Papers';
 import Cards from 'src/muiComponents/Cards';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Avatar from '@mui/material/Avatar';
-import { blue } from '@mui/material/colors';
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import Avatar from '@mui/material/Avatar';
+// import { blue } from '@mui/material/colors';
 
 function Message({ msgObj, isOwner, userObj, isLoggedIn, counter, setCounter, setValue }) {
   const [num, setNum] = useState(null)
