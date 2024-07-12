1357b1e86082d7e60adb7f80292ff7dffe846f26
changbea <charlie0421@naver.com>
2024-07-13 05:13:23 +0900

    add

--- src/lottiesAnimation/Lotties.tsx
+++ src/lottiesAnimation/Lotties.tsx
@@ -4,14 +4,14 @@ import Lottie from 'lottie-react'
 import rain from 'src/assets/Animation.json'
 
 function Lotties() {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: rain,
-        rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
-        }
-    };
+    // const defaultOptions = {
+    //     loop: true,
+    //     autoplay: true,
+    //     animationData: rain,
+    //     rendererSettings: {
+    //       preserveAspectRatio: "xMidYMid slice"
+    //     }
+    // };
     
     return (
         // <Lottie options={defaultOptions} height={400} width={400} />
